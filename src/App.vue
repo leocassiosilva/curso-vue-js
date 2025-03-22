f76d2ad20aadaab341dbd49c1f7012aa22abe724
leocassiosilva <leocassiosilva1234@gmail.com>
2025-03-22 16:51:41 -0300

    diretivas v-for

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,10 @@
 <script setup>
 //Importando o componente
 // import  Exemplo01  from "./components/Exemplo01Interpolacao.vue";
-import Exemplo02 from "./components/Exemplo02Condicional.vue";
-
+// import Exemplo02 from "./components/Exemplo02Condicional.vue";
+import Exemplo03 from "./components/Exemplo03Laco.vue";
 </script>
 
 <template>
-  <Exemplo02></Exemplo02>
+  <Exemplo03></Exemplo03>
 </template>
