4fd2a26a596ca1970496280cd51ab133df0e0baa
leocassiosilva <leocassiosilva1234@gmail.com>
2025-03-22 16:56:40 -0300

    diretivas v-html

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,10 @@
 //Importando o componente
 // import  Exemplo01  from "./components/Exemplo01Interpolacao.vue";
 // import Exemplo02 from "./components/Exemplo02Condicional.vue";
-import Exemplo03 from "./components/Exemplo03Laco.vue";
+// import Exemplo03 from "./components/Exemplo03Laco.vue";
+import Exemplo04 from "./components/Exemplo04Html.vue";
 </script>
 
 <template>
-  <Exemplo03></Exemplo03>
+  <Exemplo04></Exemplo04>
 </template>
