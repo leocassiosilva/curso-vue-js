00a6cb7cdbab09bca51ee6d8d69d3e699b35d058
leocassiosilva <leocassiosilva1234@gmail.com>
2025-03-22 19:14:39 -0300

    diretivas v-model

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,10 @@
 // import  Exemplo01  from "./components/Exemplo01Interpolacao.vue";
 // import Exemplo02 from "./components/Exemplo02Condicional.vue";
 // import Exemplo03 from "./components/Exemplo03Laco.vue";
-import Exemplo04 from "./components/Exemplo04Html.vue";
+// import Exemplo04 from "./components/Exemplo04Html.vue";
+import Exemplo05 from "./components/Exemplo05Model.vue";
 </script>
 
 <template>
-  <Exemplo04></Exemplo04>
+  <Exemplo05></Exemplo05>
 </template>
