7c8e92e00ed483b58c08b54d573c7fd138ec76ea
leocassiosilva <leocassiosilva1234@gmail.com>
2025-03-30 10:35:39 -0300

    interligação de classses

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,10 @@
 // import Exemplo02 from "./components/Exemplo02Condicional.vue";
 // import Exemplo03 from "./components/Exemplo03Laco.vue";
 // import Exemplo04 from "./components/Exemplo04Html.vue";
-import Exemplo05 from "./components/Exemplo05Model.vue";
+// import Exemplo05 from "./components/Exemplo05Model.vue";
+import Exemplo06 from "./components/Exemplo06Classe.vue";
 </script>
 
 <template>
-  <Exemplo05></Exemplo05>
+  <Exemplo06></Exemplo06>
 </template>
---
+++ src/components/Exemplo06Classe.vue
@@ -0,0 +1,23 @@
+<!--Script-->
+<script setup>
+    //variavel
+    let validacao = true; 
+
+</script>
+
+<!--Template-->
+<style>
+    .ok {
+        color: green;
+    }
+
+    .falha {
+        color: red;
+    }
+
+</style>
+
+
+<template>
+    <p :class="validacao ? 'ok': 'falha'">Utilizando a diretiva class</p>
+</template>
